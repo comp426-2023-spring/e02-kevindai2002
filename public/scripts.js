b94bb21a50d97d86c2e92522cb73a5eac7b1990b
Kevin Dai <kevindai@email.unc.edu>
2023-05-09 14:17:09 -0400

    sub1

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,87 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+
+function showHideShots() {
+    let check = document.getElementById("opponent");
+    let rpsGame = document.getElementById("rps").checked;
+  
+    if (check.checked) {
+      if (rpsGame) {
+        $(".input-container.rps").show();
+        $(".input-container.rpsls").hide(); // Hide Lizard and Spock when playing RPS against an opponent
+        $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors']").show(); // Show Rock, Paper, and Scissors labels
+        $("label[class='input-container' for='Lizard'], label[class='input-container' for='Spock']").hide();
+      } else {
+        $(".input-container.rps, .input-container.rpsls").show();
+        $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors'], label[class='input-container' for='Lizard'], label[class='input-container' for='Spock']").show(); // Hide the labels      // Show all labels
+      }
+    } else {
+      $(".input-container.rps, .input-container.rpsls").hide();
+  
+      $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors'], label[class='input-container' for='Lizard'], label[class='input-container' for='Spock']").hide(); // Hide the labels
+    }
+  
+    // Hide the radio inputs and show only the input-container elements
+    $("input[type='radio'][name='shot']").hide();
+    $(".input-container").removeClass("selected");
+    $(".input-container").css("display", "inline-flex");
+  }
+  
+  function startOver() {
+    document.getElementById('userinput').reset();
+    document.getElementById('results').innerHTML = '';
+    showHideShots();
+  }
+  
+  function resetShotSelection() {
+      let firstShot = 'Rock';
+  
+    // Reset the selected shot to a random valid option for the chosen game
+    $('input[type=radio][name=shot]').prop('checked', false);
+    $(`input[type=radio][name=shot][value=${firstShot}]`).prop('checked', true);
+  }
+  
+  function getRandomShot(game) {
+    const availableShots = {
+      rps: ['Rock', 'Paper', 'Scissors'],
+      rpsls: ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+    };
+  
+    const shots = availableShots[game];
+    return shots[Math.floor(Math.random() * shots.length)];
+  }
+  
+  async function playGame() {
+    let game = $('input[type=radio][name=game]:checked').val();
+    let shot = $('input[type=radio][name=shot]:checked').val();
+  
+    // Check if the user has not selected an opponent
+    let opponentCheckbox = document.getElementById('opponent');
+    if (!opponentCheckbox.checked) {
+      shot = getRandomShot(game); // Set the user's shot to a random shot
+    }
+  
+    let baseurl = window.location.href + 'app/';
+    console.log(baseurl);
+    let url = baseurl + game + '/play/' + shot;
+    console.log(url);
+  
+    let response = await fetch(url);
+    let result = await response.json();
+    console.log(result);
+  
+    // Display the results on the page
+    let resultsDiv = document.getElementById('results');
+    if (result.error) {
+      resultsDiv.innerHTML = result.error;
+    } else {
+      let you = `You: ${result.player}`;
+      let opponent = `Opponent: ${result.opponent}`;
+      let outcome = `Result: ${result.result}`;
+      resultsDiv.innerHTML = `${you}<br>${opponent}<br>${outcome}`;
+    }
+  }
+  
+  
--- server.js
+++ server.js
@@ -102,3 +102,66 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+
+import { play_game } from "./lib/rpsls.js";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const rps_choices = ["rock", "paper", "scissors"];
+const rpsls_choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+//Get the root endpoint of your app
+app.get('/app', function(req, res) {
+    res.status(200).send("200 OK");
+});
+
+//Play RPS
+app.get('/app/rps', function(req, res) {
+    res.status(200).send(JSON.stringify(play_game(rps_choices)));
+});
+
+//Play RPSLS
+app.get('/app/rpsls', function(req, res) {
+    res.status(200).send(JSON.stringify(play_game(rpsls_choices)));
+});
+
+//Play RPS against an opponent (URLEncoded data body)
+app.get('/app/rps/play', function(req, res) {
+    res.status(200).send(JSON.stringify(play_game(rps_choices,req.query.shot)));
+});
+
+//Play RPSLS against an opponent (URLEncoded data body)
+app.get('/app/rpsls/play', function(req, res) {
+    res.status(200).send(JSON.stringify(play_game(rpsls_choices,req.query.shot)));
+});
+
+//Play RPS against an opponent (JSON data body)
+app.post('/app/rps/play', function(req, res) {
+    res.status(200).send(JSON.stringify(play_game(rps_choices,req.body.shot)));
+});
+
+//Play RPSLS against an opponent (JSON data body)
+app.post('/app/rpsls/play', function(req, res) {
+    res.status(200).send(JSON.stringify(play_game(rpsls_choices,req.body.shot)));
+});
+
+//Play RPS against an opponent (parameter endpoint)
+app.get('/app/rps/play/:shot', function(req, res) {
+    res.status(200).send(JSON.stringify(play_game(rps_choices,req.params.shot)));
+});
+
+//Play RPSLS against an opponent (parameter endpoint)
+app.get('/app/rpsls/play/:shot', function(req, res) {
+    res.status(200).send(JSON.stringify(play_game(rpsls_choices,req.params.shot)));
+});
+
+//Call a nonexistent endpoint
+app.get('*', function(req, res){
+    res.status(404).send('404 NOT FOUND');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
